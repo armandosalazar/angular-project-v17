f28757a5a0fedeeb847601f4072abe452787f974
Armando Salazar <armandosalazar351@gmail.com>
2024-03-01 22:30:26 -0600

    feat: update app component and add post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private num: number = 17;
-  public title: string = `angular-project-v${this.num}`;
+  private num: number = 2;
+  public title: string = `Noticia ${this.num}`;
 }
